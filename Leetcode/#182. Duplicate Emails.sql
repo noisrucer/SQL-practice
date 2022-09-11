6004b154569618288b62c83a35dc2fcd70c7409f
Jason Lee <changjin9792@gmail.com>
2022-09-11 11:05:23 +0800

    Added PROBS

--- Leetcode/#182. Duplicate Emails.sql
+++ Leetcode/#182. Duplicate Emails.sql
@@ -1,3 +1,20 @@
 # 1st Solution
-SELECT DISTINCT p1.email as Email FROM Person p1, Person p2
-WHERE p1.id <> p2.id AND p1.email = p2.email;
+# SELECT DISTINCT p1.email as Email FROM Person p1, Person p2
+# WHERE p1.id <> p2.id AND p1.email = p2.email;
+
+# 2nd Solution
+# SELECT DISTINCT p1.email as Email FROM Person p1, Person p2
+# WHERE p1.id <> p2.id AND p1.email = p2.email;
+
+# 3rd Solution
+# SELECT p1.email as Email FROM Person p1
+# GROUP BY p1.email
+# HAVING COUNT(p1.email) <> 1;
+
+# 4th Solution
+# WITH c AS (
+#     SELECT email, COUNT(*) as NUM FROM Person GROUP BY email
+# )
+# SELECT email FROM c WHERE num>1;
+
+
